e2beaa0841f20bd14a58e3ba74feadbd64f74807
DZ8540 <103264577+OnPlaix@users.noreply.github.com>
2022-06-22 16:41:31 +0300

    Refactor: Refactored district seeder

--- database/seeders/District.ts
+++ database/seeders/District.ts
@@ -1,11 +1,38 @@
+import District from 'App/Models/District'
 import Logger from '@ioc:Adonis/Core/Logger'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { DistrictFactory } from 'Database/factories'
 
 export default class DistrictSeeder extends BaseSeeder {
   public async run () {
     try {
-      await DistrictFactory.createMany(30)
+      // await DistrictFactory.createMany(30)
+
+      await District.createMany([
+        {
+          city: 'Казань',
+          name: 'Ново-Савиновский',
+        },
+        {
+          city: 'Казань',
+          name: 'Советский',
+        },
+        {
+          city: 'Казань',
+          name: 'Московский',
+        },
+        {
+          city: 'Казань',
+          name: 'Кировский',
+        },
+        {
+          city: 'Казань',
+          name: 'Авиастроительный',
+        },
+        {
+          city: 'Казань',
+          name: 'Приволжский',
+        },
+      ])
     } catch (err: any) {
       Logger.error(err)
     }
